012cded92a6d6d80423a062873ee636e46382df4
mecene phrygien <mecenephrygien@gmail.com>
2025-11-21 09:56:35 +0400

    component boutique

---
+++ app/Livewire/Boutiques/Boutique.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Boutiques;
+
+use Livewire\Component;
+
+class Boutique extends Component
+{
+    public function render()
+    {
+        return view('livewire.boutiques.boutique');
+    }
+}
