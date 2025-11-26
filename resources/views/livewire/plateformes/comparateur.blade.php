8a38b930bf4d279d050ba894e063441ff87e4b14
mecene phrygien <mecenephrygien@gmail.com>
2025-11-26 12:02:35 +0400

    tst

--- resources/views/livewire/plateformes/comparateur.blade.php
+++ resources/views/livewire/plateformes/comparateur.blade.php
@@ -548,7 +548,7 @@ public function highlightMatchingTerms($text)
                 <li>
                     <div class="flex items-center text-sm">
                         <a href="#" class="font-medium text-gray-500 hover:text-gray-900">
-                            {{ $mydata['product'][0]->vendor }}
+                            Vendor
                         </a>
                         <svg viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" class="ml-2 size-5 shrink-0 text-gray-300">
                             <path d="M5.555 17.776l8-16 .894.448-8 16-.894-.448z" />
