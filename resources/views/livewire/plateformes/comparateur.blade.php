7f581038c26028e605c391aaca097a6d2638ea6e
mecene phrygien <mecenephrygien@gmail.com>
2025-11-26 12:01:25 +0400

    tst

--- resources/views/livewire/plateformes/comparateur.blade.php
+++ resources/views/livewire/plateformes/comparateur.blade.php
@@ -16,14 +16,7 @@ new class extends Component {
 
     public function mount($name, $id)
     {
-        $this->mydata = $this->getCompetitorPrice($name);
-        //$this->getOneProductDetails($id);
-    
-        //dd($this->mydata);
-        //dd($this->myadata['product'][0]->vendor);
-        //$this->getOneProductDetails($id);
-    
-        $this->id = $id;
+        $this->getCompetitorPrice($name);
     }
 
     public function getOneProductDetails($entity_id){
@@ -555,8 +548,8 @@ public function highlightMatchingTerms($text)
                 <li>
                     <div class="flex items-center text-sm">
                         <a href="#" class="font-medium text-gray-500 hover:text-gray-900">
-    {{ $mydata['product'][0]->vendor }}
-</a>
+                            {{ $mydata['product'][0]->vendor }}
+                        </a>
                         <svg viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" class="ml-2 size-5 shrink-0 text-gray-300">
                             <path d="M5.555 17.776l8-16 .894.448-8 16-.894-.448z" />
                         </svg>
