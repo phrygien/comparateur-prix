cef8d16326be452dfef395549505ca0f42a35f93
mecene phrygien <mecenephrygien@gmail.com>
2025-11-26 12:55:05 +0400

    details product

--- resources/views/livewire/plateformes/detail.blade.php
+++ resources/views/livewire/plateformes/detail.blade.php
@@ -78,7 +78,7 @@ new class extends Component {
 
             $result = DB::connection('mysqlMagento')->select($dataQuery, [$entity_id]);
 
-            $this->product = $result;
+            $this->product = "hello";
 
         } catch (\Throwable $e) {
             \Log::error('Error loading products:', [
