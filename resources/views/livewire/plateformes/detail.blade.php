9527f7cd492ca7327397b6442bb472ef9299d5ae
mecene phrygien <mecenephrygien@gmail.com>
2025-11-27 09:56:51 +0400

    fix header title

--- resources/views/livewire/plateformes/detail.blade.php
+++ resources/views/livewire/plateformes/detail.blade.php
@@ -98,15 +98,7 @@ new class extends Component {
 <div class="w-full px-4 py-6 sm:px-6 lg:grid lg:grid-cols-2 lg:gap-x-10 lg:px-10">
 
 
-<x-header title="{{ utf8_encode($this->product->title) ?? 'N/A' }}" subtitle="{{ utf8_encode($this->product->vendor) ?? 'N/A' }}" no-separator >
-    <x-slot:middle class="!justify-end">
-        <x-input icon="o-bolt" placeholder="Search..." />
-    </x-slot:middle>
-    <x-slot:actions>
-        <x-button icon="o-funnel" />
-        <x-button icon="o-plus" class="btn-primary" />
-    </x-slot:actions>
-</x-header>
+<x-header title="{{ utf8_encode($this->product->title) ?? 'N/A' }}" subtitle="{{ utf8_encode($this->product->vendor) ?? 'N/A' }}" no-separator />
 
 
 <!-- Product image - Left column -->
