c070757039a2e59e9d307d16e68771903cc4d82b
mecene phrygien <mecenephrygien@gmail.com>
2025-11-28 09:06:08 +0400

    update login

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -39,7 +39,7 @@
 
                 {{-- User --}}
                 @if($user = auth()->user())
-                    <x-menu-separator />
+                    {{-- <x-menu-separator /> --}}
 
                     <x-list-item :item="$user" value="name" sub-value="email" no-separator no-hover class="-mx-2 !-my-2 rounded">
                         <x-slot:actions>
