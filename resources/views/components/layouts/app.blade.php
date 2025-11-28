fec64114a08e2ab76388b427cdb741c8adc89804
mecene phrygien <mecenephrygien@gmail.com>
2025-11-28 09:05:08 +0400

    update login

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -32,7 +32,7 @@
         <x-slot:sidebar drawer="main-drawer" collapsible class="bg-base-100 lg:bg-inherit">
 
             {{-- BRAND --}}
-            <x-app-brand class="px-5 pt-4" />
+            {{-- <x-app-brand class="px-5 pt-4" /> --}}
 
             {{-- MENU --}}
             <x-menu activate-by-route>
