5eeaab280b8cf343c8f6cc017a0e4957dc53f2e8
mecene phrygien <mecenephrygien@gmail.com>
2025-12-03 13:42:06 +0400

    test

--- resources/views/livewire/plateformes/detail.blade.php
+++ resources/views/livewire/plateformes/detail.blade.php
@@ -267,18 +267,15 @@ new class extends Component {
                         Produit à comparer sur le concurrent
                     </h3>
                     
-                    <!-- Badge "hide" comme ul badge -->
-                    <div class="flex items-center space-x-2">
-                        <span class="text-xs text-gray-500">Masquer la barre</span>
-                        <button @click="toggleBarVisibility()" 
-                                class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-red-100 text-red-800 hover:bg-red-200 transition-colors"
-                                title="Masquer la barre flottante">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3 mr-1" viewBox="0 0 20 20" fill="currentColor">
-                                <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
-                            </svg>
-                            Hide
-                        </button>
-                    </div>
+                    <!-- Badge "hide" positionné à droite -->
+                    <button @click="toggleBarVisibility()" 
+                            class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-red-100 text-red-800 hover:bg-red-200 transition-colors"
+                            title="Masquer la barre flottante">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3 mr-1" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
+                        </svg>
+                        Hide
+                    </button>
                 </div>
                 
                 <div class="flex items-center justify-between">
