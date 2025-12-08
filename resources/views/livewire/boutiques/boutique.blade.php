434ef42f9ab0a884967614164ebd099ab5216d9f
mecene phrygien <mecenephrygien@gmail.com>
2025-12-08 11:49:16 +0400

    loading prix

--- resources/views/livewire/boutiques/boutique.blade.php
+++ resources/views/livewire/boutiques/boutique.blade.php
@@ -330,9 +330,9 @@
                 <svg class="w-16 h-16 text-primary animate-pulse" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                 </svg>
-                <div class="absolute inset-0 flex items-center justify-center">
+                {{-- <div class="absolute inset-0 flex items-center justify-center">
                     <div class="loading loading-spinner loading-md text-primary"></div>
-                </div>
+                </div> --}}
             </div>
             
             <!-- Texte principal -->
