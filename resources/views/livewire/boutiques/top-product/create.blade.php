b300d4294a73d2558e699df0a112196c50724361
mecene phrygien <mecenephrygien@gmail.com>
2026-01-20 16:00:43 +0400

    create list

--- resources/views/livewire/boutiques/top-product/create.blade.php
+++ resources/views/livewire/boutiques/top-product/create.blade.php
@@ -7,5 +7,13 @@ new class extends Component {
 }; ?>
 
 <div class="mx-auto max-w-5xl">
-    //
+    <x-header title="Creer la liste a commpararer" separator>
+        <x-slot:middle class="!justify-end">
+            <x-input icon="o-bolt" placeholder="Search..." />
+        </x-slot:middle>
+        <x-slot:actions>
+            <x-button icon="o-funnel" />
+            <x-button icon="o-plus" class="btn-primary" />
+        </x-slot:actions>
+    </x-header>
 </div>
