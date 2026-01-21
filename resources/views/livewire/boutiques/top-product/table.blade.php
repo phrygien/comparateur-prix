36220f29ae9c14699779b2f1c6ff9d76e80bbd3a
Mecene <mecenephrygien@gmail.com>
2026-01-21 20:48:43 +0400

    debug:infiniti scroll

--- resources/views/livewire/boutiques/top-product/table.blade.php
+++ resources/views/livewire/boutiques/top-product/table.blade.php
@@ -15,7 +15,7 @@ new class extends Component {
     public bool $loadingMore = false;
     public bool $hasMore = true;
     public int $page = 1;
-    public int $perPage = 20;
+    public int $perPage = 5;
 
     // Cache
     protected $cacheTTL = 3600;
