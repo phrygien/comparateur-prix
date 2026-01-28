9a7fa7e959b5b771b9930b47d1724bb3469a8144
Hary Ny Aina RAJAONAH <hrajaonah@techmode.local>
2026-01-28 09:32:02 +0400

    enlever les accents

--- app/Livewire/Boutiques/Boutique.php
+++ app/Livewire/Boutiques/Boutique.php
@@ -629,6 +629,8 @@ class Boutique extends Component
                 LIMIT ? OFFSET ?
             ";
 
+            dd($dataQuery);
+
             $params[] = $perPage;
             $params[] = $offset;
 
