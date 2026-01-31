7adebaaabfd1edb617ab56b4ecce54734665ffdd
Mecene <mecenephrygien@gmail.com>
2026-01-31 17:25:18 +0400

    change btn style

--- resources/views/livewire/scraped_products/list.blade.php
+++ resources/views/livewire/scraped_products/list.blade.php
@@ -457,9 +457,9 @@ new class extends Component {
             <!-- Boutons d'action -->
             <div class="flex justify-end items-center mt-3 gap-2">
                 @if($showResults)
-                    <x-button wire:click="resetFilter" icon="o-x-mark" label="Réinitialiser" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900" />
+                    <x-button wire:click="resetFilter" label="Réinitialiser" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900" />
                 @endif
-                <x-button wire:click="applyFilter" icon="o-funnel" label="Appliquer les filtres"
+                <x-button wire:click="applyFilter" icon="o-magnifying-glass" label="Appliquer les filtres"
                     class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" spinner />
                 @if($showResults && $totalResults > 0)
                     <x-button wire:click="exportCsv" icon="o-arrow-down-tray" label="Exporter CSV ({{ $totalResults }})"
