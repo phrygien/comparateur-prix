e9177a93f38e28d70ebca3dee45256aed970b925
mecene phrygien <mecenephrygien@gmail.com>
2026-02-10 11:06:16 +0400

    function importer

--- resources/views/livewire/rankings/import-file.blade.php
+++ resources/views/livewire/rankings/import-file.blade.php
@@ -391,6 +391,7 @@ new class extends Component {
                             <td>{{ $histo->created_at->format('d/m/Y H:i') }}</td>
                             <td>
                                 <div class="flex gap-2">
+                                    <x-button icon="o-eye" class="btn-primary btn-sm btn-soft" link="/ranking-result" />
                                     <button 
                                         wire:click="telecharger({{ $histo->id }})"
                                         class="btn btn-sm btn-ghost"
