34d6b4b79f1d2a3b106df368dcec5ec7be55b2f2
mecene phrygien <mecenephrygien@gmail.com>
2026-02-10 10:19:09 +0400

    create new route

--- resources/views/livewire/rankings/import-file.blade.php
+++ resources/views/livewire/rankings/import-file.blade.php
@@ -9,5 +9,13 @@ new class extends Component {
 <div class="max-w-7xl">
     <x-header title="Importer" subtitle="Importer le fichier de top product" separator />
 
-    <x-file wire:model="file" label="Ranking File" hint="Format xlsx" accept="application/xlsx" />
+
+    <x-form wire:submit="importer">
+
+            <x-file wire:model="file" label="Ranking File" hint="Format xlsx" accept="application/xlsx" />
+        <x-slot:actions>
+            <x-button label="Importer" class="btn-primary" type="submit" spinner="importer" />
+        </x-slot:actions>
+    </x-form>
+
 </div>
