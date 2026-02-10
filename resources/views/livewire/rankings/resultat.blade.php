3a34ef943f02bf3b5cba8daa40522ab9c3908808
Hary Ny Aina RAJAONAH <hrajaonah@techmode.local>
2026-02-10 15:24:41 +0400

    calcul de la perte en moyenne

--- resources/views/livewire/rankings/resultat.blade.php
+++ resources/views/livewire/rankings/resultat.blade.php
@@ -300,12 +300,12 @@ new class extends Component {
     </x-header>
 
     <div class="grid grid-cols-4 gap-4">
-        <x-stat title="Competitif" value="{{ number_format($somme_gain, 0, ',', ' ') }} € " tooltip="Hello" color="text-primary" />
+        <x-stat title="Competitif en moyenne" value="{{ number_format($somme_gain, 0, ',', ' ') }} € " tooltip="Hello" color="text-primary" />
     
         <x-stat title="Competitif ( % )" description=""
             class="text-green-500" color="text-green-500" colvalue="{{ number_format($percentage_gain_marche, 2, ',', ' ') }} %" icon="o-arrow-trending-up" />
     
-        <x-stat title="Lacune" value="{{ number_format($somme_perte, 0, ',', ' ') }} €"
+        <x-stat title="Lacune en moyenne" value="{{ number_format($somme_perte, 0, ',', ' ') }} €"
             tooltip-left="{{ number_format($somme_perte, 0, ',', ' ') }}" />
     
         <x-stat title="Lacune (%)" description=""
