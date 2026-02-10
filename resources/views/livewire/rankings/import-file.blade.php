1e805767d3c5099823e7d93785a5fffb7c34685b
mecene phrygien <mecenephrygien@gmail.com>
2026-02-10 10:14:35 +0400

    create new route

--- resources/views/livewire/rankings/import-file.blade.php
+++ resources/views/livewire/rankings/import-file.blade.php
@@ -6,6 +6,8 @@ new class extends Component {
     //
 }; ?>
 
-<div>
-    //
+<div class="max-w-7xl">
+    <x-header title="Importer" subtitle="Importer le fichier de top product" separator />
+
+    <x-file wire:model="file" label="Ranking File" hint="Format xlsx" accept="application/xlsx" />
 </div>
