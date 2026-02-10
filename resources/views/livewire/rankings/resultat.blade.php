c164815e40448c25c8316f44c34ef2208f1b452c
Hary Ny Aina RAJAONAH <hrajaonah@techmode.local>
2026-02-10 16:02:59 +0400

    card

--- resources/views/livewire/rankings/resultat.blade.php
+++ resources/views/livewire/rankings/resultat.blade.php
@@ -302,7 +302,7 @@ new class extends Component {
     <div class="grid grid-cols-4 gap-4">
         <x-stat title="Competitif en moyenne" value="{{ number_format( ($somme_gain/$perPage) , 2, ',', ' ') }} € " tooltip="" color="text-primary" />
     
-        <x-stat title="Competitif ( % )" description="" colvalue="{{ number_format($percentage_gain_marche, 2, ',', ' ') }} %" icon="o-arrow-trending-up" />
+        <x-stat title="Competitif ( % )" description="" value="{{ number_format($percentage_gain_marche, 2, ',', ' ') }} %" icon="o-arrow-trending-up" />
     
         <x-stat title="Lacune en moyenne" value="{{ number_format( ($somme_perte/$perPage) , 2, ',', ' ') }} €" tooltip-left="" />
     
