16342277d2fcc1cf2d589732c993289574825a88
mecene phrygien <mecenephrygien@gmail.com>
2026-02-11 13:42:25 +0400

    store file hash

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -57,9 +57,9 @@
 
                     <!-- Sidebar content -->
                     <div class="flex grow flex-col gap-y-5 overflow-y-auto bg-indigo-600 px-6 pb-2">
-                        <div class="flex h-16 shrink-0 items-center">
+                        {{-- <div class="flex h-16 shrink-0 items-center">
                             <x-app-brand />
-                        </div>
+                        </div> --}}
                         <nav class="flex flex-1 flex-col">
                             <ul role="list" class="flex flex-1 flex-col gap-y-7">
                                 <li>
@@ -121,9 +121,9 @@
              :class="sidebarMinimized ? 'lg:w-20' : 'lg:w-72'">
             <div class="flex grow flex-col gap-y-5 overflow-y-auto bg-indigo-600 px-6">
                 <div class="flex h-16 shrink-0 items-center justify-between">
-                    <div x-show="!sidebarMinimized" x-transition>
+                    {{-- <div x-show="!sidebarMinimized" x-transition>
                         <x-app-brand />
-                    </div>
+                    </div> --}}
                     <button @click="sidebarMinimized = !sidebarMinimized" 
                             class="p-1.5 rounded-md text-indigo-200 hover:text-white hover:bg-indigo-700 transition-colors"
                             :class="sidebarMinimized && 'mx-auto'">
@@ -181,7 +181,7 @@
                                             <svg class="size-6 shrink-0 {{ request()->is('import-file*') ? 'text-white' : 'text-indigo-200 group-hover:text-white' }}" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                                 <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z" />
                                             </svg>
-                                            <span x-show="!sidebarMinimized" x-transition>Importer Ranking File</span>
+                                            <span x-show="!sidebarMinimized" x-transition>Importer Top produit</span>
                                         </a>
                                     </li>
                                 @endif
