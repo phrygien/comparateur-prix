3a245ed9d14299690063eb6084b69c27077a118b
mecene phrygien <mecenephrygien@gmail.com>
2026-02-11 11:05:52 +0400

    edit details

--- resources/views/livewire/plateformes/comparateur/typesense.blade.php
+++ resources/views/livewire/plateformes/comparateur/typesense.blade.php
@@ -65,8 +65,7 @@ new class extends Component {
 
 }; ?>
 
-<div >
-    <div class="w-full max-w-5xl mx-auto p-6">
+<div class="w-full max-w-7xl mx-auto p-6">
     <livewire:plateformes.detail :id="$id" />
 
     @if($this->products->count() > 0)
@@ -183,5 +182,4 @@ new class extends Component {
             </p>
         </div>
     @endif
-    </div>
 </div>
